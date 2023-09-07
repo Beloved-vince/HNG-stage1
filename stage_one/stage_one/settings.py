af1a6f2ca854a0c9e0654d29f765091893558752
Beloved-vince <vinceoludare@gmail.com>
2023-09-07 13:02:44 +0100

    .gitignore

--- stage_one/stage_one/settings.py
+++ stage_one/stage_one/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-5s&(-k+lor$juregexu8jwuky7cb2s%+w$*x4wb=#fpc8-0hlk'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
